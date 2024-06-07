4fac54a2365a828841c4480114898355d8e35e07
里見 凱夫 <satomiyo@fsi.co.jp>
2024-06-07 17:31:30 +0900

    tweak

--- keyboards/omkbd/ergodash/rev1/keymaps/sat_via/config.h
+++ keyboards/omkbd/ergodash/rev1/keymaps/sat_via/config.h
@@ -25,3 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
+
+#define TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY
